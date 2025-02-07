ebb231cd9c4455fa195687742a81379b51b31ddb
PierreL-Data <lefebvre.pierre59@gmail.com>
2025-02-07 11:07:31 +0100

    Update Streamlit3.py

--- Streamlit3.py
+++ Streamlit3.py
@@ -24,64 +24,67 @@ authenticator = Authenticate(
     "cookie key", # La clé du cookie, un str quelconque
     30, # Le nombre de jours avant que le cookie expire 
 )
+# Sidebar pour l'authentification
 with st.sidebar:
-   authenticator.login()
+    authenticator = stauth.Authenticate(...)  # Remplace avec ta config d'authentification
+    authenticator.login()
 
-   def accueil():
-         st.title("Bienvenu sur le contenu réservé aux utilisateurs connectés")
-   
-   
-   if st.session_state["authentication_status"]:
-     accueil()
-     # Le bouton de déconnexion
-     authenticator.logout("Déconnexion")
-         # Création du menu qui va afficher les choix qui se trouvent dans la variable options
-     selection = option_menu(
-                  menu_title=None,
-                  options = ["Accueil", "Photos"]
-              )
-            # Using object notation
-     add_selectbox = st.sidebar.selectbox(
-         "How would you like to be contacted?",
-         ("Email", "Home phone", "Mobile phone")
-     )
-      
-      # Using "with" notation
-      with st.sidebar:
-          add_radio = st.radio(
-              "Choose a shipping method",
-              ("Standard (5-15 days)", "Express (2-5 days)")
-          )
-        
-     if selection == "Accueil":
-         st.write("Bienvenue sur la page d'accueil !")
-      
-     elif selection == "Photos":
-         st.write("Bienvenue sur mon album photo")
-          
-         col1, col2, col3 = st.columns(3)
-      
-         with col1:
-             st.header("C'est ")
-             st.image("https://giphy.com/gifs/thepmc-dg0hVakNxI0LaIQDQm")
-      
-         with col2:
-             st.header("Vendre")
-             st.image("https://giphy.com/gifs/its-friday-393kszFi2PuCEopURN")
-      
-         with col3:
-             st.header("di!!!!!!")
-             st.image("https://giphy.com/gifs/butleruniversity-happy-dog-puppy-u2bR9FEoMztbrklifS")
-   
-   elif st.session_state["authentication_status"] is False:
-       st.error("L'username ou le password est/sont incorrect")
-   elif st.session_state["authentication_status"] is None:
-       st.warning('Les champs username et mot de passe doivent être remplis. Merci de ne vous relire dans les quêtes. indice : Mettre utilisateur et utilisateurMDP pour se connecter.')
+# Fonction d'accueil
+def accueil():
+    st.title("Bienvenue sur le contenu réservé aux utilisateurs connectés")
 
+# Vérification de l'authentification
+if "authentication_status" in st.session_state:
+    if st.session_state["authentication_status"]:
+        accueil()
 
-       
+        # Bouton de déconnexion
+        authenticator.logout("Déconnexion")
 
+        # Création du menu de navigation
+        selection = option_menu(
+            menu_title=None,
+            options=["Accueil", "Photos"]
+        )
 
+        # Contenu de la page sélectionnée
+        if selection == "Accueil":
+            st.write("Bienvenue sur la page d'accueil !")
+
+        elif selection == "Photos":
+            st.write("Bienvenue sur mon album photo")
+
+            col1, col2, col3 = st.columns(3)
+
+            with col1:
+                st.header("C'est")
+                st.image("https://media.giphy.com/media/dg0hVakNxI0LaIQDQm/giphy.gif")
+
+            with col2:
+                st.header("Vendre")
+                st.image("https://media.giphy.com/media/393kszFi2PuCEopURN/giphy.gif")
+
+            with col3:
+                st.header("di!!!!!!")
+                st.image("https://media.giphy.com/media/u2bR9FEoMztbrklifS/giphy.gif")
+
+        # Options supplémentaires dans la barre latérale
+        with st.sidebar:
+            add_selectbox = st.selectbox(
+                "How would you like to be contacted?",
+                ("Email", "Home phone", "Mobile phone")
+            )
+
+            add_radio = st.radio(
+                "Choose a shipping method",
+                ("Standard (5-15 days)", "Express (2-5 days)")
+            )
+
+    elif st.session_state["authentication_status"] is False:
+        st.error("L'username ou le password est incorrect.")
+
+    elif st.session_state["authentication_status"] is None:
+        st.warning("Les champs username et mot de passe doivent être remplis.")
 
 
 
