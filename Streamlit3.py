35485e745b03a27eebd8900c3ae1eba2597a7b78
PierreL-Data <lefebvre.pierre59@gmail.com>
2025-02-07 10:49:18 +0100

    Update Streamlit3.py

--- Streamlit3.py
+++ Streamlit3.py
@@ -53,19 +53,21 @@ with st.sidebar:
 
    elif selection == "Photos":
        st.write("Bienvenue sur mon album photo")
-      col1, col2, col3 = st.columns(3)
       
-      with col1:
-        st.header("A cat")
-        st.image("https://static.streamlit.io/examples/cat.jpg")
-      
-      with col2:
-        st.header("A dog")
-        st.image("https://static.streamlit.io/examples/dog.jpg")
-      
-      with col3:
-        st.header("An owl")
-        st.image("https://static.streamlit.io/examples/owl.jpg")
+if selection == "Photos":
+   col1, col2, col3 = st.columns(3)
+   
+   with col1:
+     st.header("A cat")
+     st.image("https://static.streamlit.io/examples/cat.jpg")
+   
+   with col2:
+     st.header("A dog")
+     st.image("https://static.streamlit.io/examples/dog.jpg")
+   
+   with col3:
+     st.header("An owl")
+     st.image("https://static.streamlit.io/examples/owl.jpg")
 
 
 
