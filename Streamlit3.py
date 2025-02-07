bba3529154980d4238c4d2548d28374ded3df009
PierreL-Data <lefebvre.pierre59@gmail.com>
2025-02-07 10:52:34 +0100

    Update Streamlit3.py

--- Streamlit3.py
+++ Streamlit3.py
@@ -56,19 +56,19 @@ if st.session_state["authentication_status"] is True:
    elif selection == "Photos":
        st.write("Bienvenue sur mon album photo")
       
-      col1, col2, col3 = st.columns(3)
-   
-      with col1:
-        st.header("A cat")
-        st.image("https://static.streamlit.io/examples/cat.jpg")
-   
-      with col2:
-        st.header("A dog")
-        st.image("https://static.streamlit.io/examples/dog.jpg")
+         col1, col2, col3 = st.columns(3)
       
-      with col3:
-        st.header("An owl")
-        st.image("https://static.streamlit.io/examples/owl.jpg")
+         with col1:
+           st.header("A cat")
+           st.image("https://static.streamlit.io/examples/cat.jpg")
+      
+         with col2:
+           st.header("A dog")
+           st.image("https://static.streamlit.io/examples/dog.jpg")
+         
+         with col3:
+           st.header("An owl")
+           st.image("https://static.streamlit.io/examples/owl.jpg")
 
 
 
