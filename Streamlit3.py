a45f91526dad71e2f8e424e6cda4f9b4db5550fa
PierreL-Data <lefebvre.pierre59@gmail.com>
2025-02-07 10:56:11 +0100

    Update Streamlit3.py

--- Streamlit3.py
+++ Streamlit3.py
@@ -49,27 +49,27 @@ if st.session_state["authentication_status"] is True:
                options = ["Accueil", "Photos"]
            )
 
-# On indique au programme quoi faire en fonction du choix
-   if selection == "Accueil":
-       st.write("Bienvenue sur la page d'accueil !")
 
-   elif selection == "Photos":
-       st.write("Bienvenue sur mon album photo")
-      
-         col1, col2, col3 = st.columns(3)
-      
-         with col1:
-           st.header("A cat")
-           st.image("https://static.streamlit.io/examples/cat.jpg")
-      
-         with col2:
-           st.header("A dog")
-           st.image("https://static.streamlit.io/examples/dog.jpg")
-         
-         with col3:
-           st.header("An owl")
-           st.image("https://static.streamlit.io/examples/owl.jpg")
 
+if selection == "Accueil":
+    st.write("Bienvenue sur la page d'accueil !")
+
+elif selection == "Photos":
+    st.write("Bienvenue sur mon album photo")
+    
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.header("A cat")
+        st.image("https://static.streamlit.io/examples/cat.jpg")
+
+    with col2:
+        st.header("A dog")
+        st.image("https://static.streamlit.io/examples/dog.jpg")
+
+    with col3:
+        st.header("An owl")
+        st.image("https://static.streamlit.io/examples/owl.jpg")
 
 
 
